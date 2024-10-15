841b68725a93e34247fed9bf63bdaa6bec7c0677
Enzo Jesuan Del Sole <enzodelsole@gmail.com>
2024-10-14 23:41:06 -0300

    Delete TP2/TP2-INTERFACES directory

--- TP2/TP2-INTERFACES/js/header.js
+++
@@ -1,7 +0,0 @@
-
-const menuHamburguesa = document.getElementById("menu-hamburguesa");
-const menuDesplegable = document.querySelector(".menu-desplegable");
-
-menuHamburguesa.addEventListener("click", ()=>{
-  menuDesplegable.classList.toggle("menu-desplegable-cerrado");
-});
