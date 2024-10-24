51904dff82a622e0786b98e135275ef873fa5f68
Agus-dx <agusdx2001@gmail.com>
2024-10-24 14:51:24 -0300

    estructura del juego

--- TP3/EntregaFinal/js/game.js
+++ TP3/EntregaFinal/js/game.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   const botonJugar = document.getElementById("juego-boton-jugar");
-const triangulo_play = document.getElementById("juego-boton-jugar-triangulo-play");
+  const triangulo_play = document.getElementById("juego-boton-jugar-triangulo-play");
+  const juego_cerrado = document.getElementById("juego-cerrado");
+  const juego_ejecutandose = document.getElementById("juego-ejecutandose");
 
 botonJugar.onmouseover = function(){
   triangulo_play.src='img/game/triangulo-play-hover.png';
@@ -8,4 +10,9 @@ botonJugar.onmouseover = function(){
 botonJugar.onmouseleave = function(){
   triangulo_play.src='img/game/triangulo-play.png';
 }
+botonJugar.onclick = function(){
+  juego_cerrado.classList.add("ocultar");
+  juego_ejecutandose.classList.remove("ocultar");
+}
+
 });
