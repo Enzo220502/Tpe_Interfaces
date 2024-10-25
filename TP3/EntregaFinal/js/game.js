2fa285a461247f0742b101970c874a6b14fe9944
Agus-dx <agusdx2001@gmail.com>
2024-10-25 14:57:34 -0300

    Nueva pestaña de juego menu y ejecutandose

--- TP3/EntregaFinal/js/game.js
+++ TP3/EntregaFinal/js/game.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const botonJugar = document.getElementById("juego-boton-jugar");
-  const triangulo_play = document.getElementById("juego-boton-jugar-triangulo-play");
+  const boton_jugar = document.getElementById("juego-btn-jugar");
+  const triangulo_play = document.getElementById("juego-btn-jugar-triangulo-play");
   const juego_cerrado = document.getElementById("juego-cerrado");
   const juego_ejecutandose = document.getElementById("juego-ejecutandose");
+  const juego_menu = document.getElementById("juego-menu");
+  const boton_comenzar = document.getElementById("juego-btn-comenzar");
 
-botonJugar.onmouseover = function(){
+boton_jugar.onmouseover = function(){
   triangulo_play.src='img/game/triangulo-play-hover.png';
 }
-botonJugar.onmouseleave = function(){
+boton_jugar.onmouseleave = function(){
   triangulo_play.src='img/game/triangulo-play.png';
 }
-botonJugar.onclick = function(){
+boton_jugar.onclick = function(){
   juego_cerrado.classList.add("ocultar");
+  juego_menu.classList.remove("ocultar");
+}
+boton_comenzar.onclick = function(){
+  juego_menu.classList.add("ocultar");
   juego_ejecutandose.classList.remove("ocultar");
 }
 
