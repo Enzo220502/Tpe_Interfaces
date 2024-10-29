fe894723c4885910029a0133acdaed1db17a8a31
Agus-dx <agusdx2001@gmail.com>
2024-10-29 17:51:58 -0300

    menu del juego

--- TP3/EntregaFinal/js/game.js
+++ TP3/EntregaFinal/js/game.js
@@ -21,4 +21,61 @@ boton_comenzar.onclick = function(){
   juego_ejecutandose.classList.remove("ocultar");
 }
 
+document.querySelectorAll('.ficha').forEach((ficha, index) => {
+  ficha.addEventListener('mouseover', () => {
+    for (let i = 0; i <= index; i++) {
+      ficha.parentElement.children[i].classList.add('ficha-hover'); // Ilumina fichas a la izquierda
+    }
+  });
+
+  ficha.addEventListener('mouseleave', () => {
+    for (let i = 0; i <= index; i++) {
+      ficha.parentElement.children[i].classList.remove('ficha-hover'); // Restaura el efecto al salir
+    }
+  });
+
+  ficha.addEventListener('click', () => {
+    for (let i = 0; i < ficha.parentElement.children.length; i++) {
+      ficha.parentElement.children[i].classList.remove('ficha-active'); // Elimina el efecto activo de todas
+    }
+    for (let i = 0; i <= index; i++) {
+      ficha.parentElement.children[i].classList.add('ficha-active'); // Restaura el efecto al salir
+    }
+  });
+});
+
+
+
+// Función para abrir el popup
+document.getElementById('ficha-selector-1').addEventListener('click', () => {
+  document.getElementById('ficha-popup-1').classList.remove('ocultar');
+});
+
+// Manejar la selección de la ficha
+document.querySelectorAll('.fichas-1').forEach(opcion => {
+  opcion.addEventListener('click', (event) => {
+      const fichaSeleccionada = event.target.getAttribute('src');
+      document.getElementById('ficha-imagen-1').src = fichaSeleccionada; // Actualiza la imagen en el selector
+      document.getElementById('ficha-popup-1').classList.add('ocultar'); // Cierra el popup
+  });
+});
+
+// Función para abrir el popup
+document.getElementById('ficha-selector-2').addEventListener('click', () => {
+  document.getElementById('ficha-popup-2').classList.remove('ocultar');
+});
+
+// Manejar la selección de la ficha
+document.querySelectorAll('.fichas-2').forEach(opcion => {
+  opcion.addEventListener('click', (event) => {
+      const fichaSeleccionada = event.target.getAttribute('src');
+      document.getElementById('ficha-imagen-2').src = fichaSeleccionada; // Actualiza la imagen en el selector
+      document.getElementById('ficha-popup-2').classList.add('ocultar'); // Cierra el popup
+  });
+});
+
+document.getElementById('ficha-selector-2').addEventListener('click', () => {
+  document.getElementById('ficha-popup-2').classList.remove('ocultar');
+});
+
 });
